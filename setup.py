a0e9209d023da087896838284b1bfd833faaefc8
Karl Tayeb <karl.tayeb7@gmail.com>
2019-03-24 20:45:39 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='assignmentgp',
+      version='1.0',
+      description='Gaussian process with latent assignment to output dimension',
+      author='Karl Tayeb',
+      author_email='karl.tayeb7@gmail.com',
+      packages=find_packages()
+      )
